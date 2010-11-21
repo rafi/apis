f6343be996b1c54a9f4b96794f632cd50e8d6ed2
Woody Gilk <woody.gilk@kohanaframework.org>
2010-11-21 09:34:28 -0600

    Added "status" demos

--- twitter/classes/controller/twitter/demo.php
+++ twitter/classes/controller/twitter/demo.php
@@ -32,6 +32,100 @@ class Controller_Twitter_Demo extends Controller_Demo {
 		$this->content = Kohana::debug($response);
 	}
 
+	public function demo_status_friends_timeline()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->friends_timeline($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_home_timeline()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->home_timeline($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_mentions()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->mentions($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_retweeted_by_me()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->retweeted_by_me($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_retweeted_to_me()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->retweeted_to_me($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_retweets_of_me()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->retweets_of_me($this->consumer, $this->token);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_public_timeline()
+	{
+		$api = Twitter::factory('status');
+
+		$response = $api->public_timeline($this->consumer);
+
+		$this->content = Kohana::debug($response);
+	}
+
+	public function demo_status_user_timeline()
+	{
+		if (Request::$method === 'POST')
+		{
+			// Get the screen name and account id from POST
+			$params = Arr::extract($_POST, array('screen_name', 'account_id'));
+
+			if ( ! $params)
+			{
+				// No parameters included
+				$this->request->redirect($this->request->uri);
+			}
+
+			$api = Twitter::factory('status');
+
+			$response = $api->user_timeline($this->consumer, $this->token, $params);
+
+			$this->content = Kohana::debug($response);
+		}
+		else
+		{
+			$this->content = View::factory('api/form')
+				->set('message', 'Enter an account ID or screen name.')
+				->set('inputs', array(
+					'Screen Name' => Form::input('screen_name'),
+					'Account ID'  => Form::input('acount_id'),
+				))
+				;
+		}
+	}
+
 	public function demo_user_show()
 	{
 		if (Request::$method === 'POST')
